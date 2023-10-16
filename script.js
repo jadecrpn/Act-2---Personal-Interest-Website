2be3460d9476f3f9f31fe58ebfc760682d83aa17
AustineBill <auztine.malic@gmail.com>
2023-10-16 14:39:25 +0800

    Updated Austine Content

--- script.js
+++ script.js
@@ -34,41 +34,63 @@ showNextLetter();
 function toggleMenu() {
     const More = document.querySelector(".Button-moreinfo");
     More.classList.toggle("open");  
-}
+}*/
 
-/*const card = document.getElementById('content');
-const moreContent = document.getElementById('moreContent');
-const toggleButton = document.getElementById('Button-moreinfo');
-
-let isShowMore = false;
-
-toggleButton.addEventListener('click', () => {
-  isShowMore = !isShowMore;
-  
-  if (isShowMore) {
-    moreContent.style.display = 'block';
-    toggleButton.textContent = 'Read Less';
-  } else {
-    moreContent.style.display = 'none';
-    toggleButton.textContent = 'Read More';
-  }
-});*/
-
-function toggleMenu() {
+/*function toggleMenu() {
  
     // Get all the elements from the page
     const points = document.getElementById("text-container");
     const showMoreText = document.getElementById("container-more");
-    const buttonText = document.getElementById("Buttton-moreinfo");
+    let buttonText = document.getElementById("Buttton-moreinfo");
 
 
-
-    if (points.style.display === "none") {
+    if (points.style.display ==  "none") {
  
         // Hide the text between the span
         // elements
         showMoreText.style.display = "none";
 
+        // Show the dots after the text
+        points.style.display = "inline";
+
+        // Change the text on button to 
+        // 'Show More'
+        buttonText.innerHTML = "Show More";
+
+        //showMoreText.style.display = showMoreText.style.display === "none" ? "block" : "none";
+    }
+
+    // If the hidden portion is revealed,
+    // we will change it back to be hidden
+    else {
+
+        // Show the text between the
+        // span elements
+        showMoreText.style.display = "inline";
+
+        // Hide the dots after the text
+        points.style.display = "none";
+
+        // Change the text on button
+        // to 'Show Less'
+        buttonText.innerHTML = "Show Less";
+    }
+}*/
+
+function toggleMenu(button) {
+ 
+    // Get all the elements from the page
+    const points = button.parentElement.querySelector("text-container");
+    const showMoreText = button.document.parentElement.querySelector("container-more");
+    const buttonText = button;
+
+
+    if (points.style.display ==  "none") {
+ 
+        // Hide the text between the span
+        // elements
+        showMoreText.style.display = "none";
+        
         // Show the dots after the text
         points.style.display = "inline";
 
@@ -93,5 +115,8 @@ function toggleMenu() {
         buttonText.innerHTML = "Show Less";
     }
 }
+
+
+
 
 
